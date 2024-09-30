8fd3f86ce80cc34ffe4ab083dc20440fea41afcb
awoodham11 <alexwoodham08@gmail.com>
2024-09-30 22:37:08 +0100

    initial commit

---
+++ DentistSystem/DentistSystem/Controllers/DentistAppController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DentistSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DentistAppController : ControllerBase
+    {
+
+        private IConfiguration _configuration;
+
+        public DentistAppController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+
+        [HttpGet]
+        [Route("GetDentists")]
+        public JsonResult GetDentists()
+        {
+            string query = "select * from dbo.dentists";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("dentistappDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource)) { 
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon)) { 
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                } 
+            }
+
+            return new JsonResult(table);
+        }
+
+
+        [HttpPost]
+        [Route("AddDentists")]
+        public JsonResult AddDentists([FromForm] string newDentists)
+        {
+            string query = "insert into dbo.dentists values(@newDentists)";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("dentistappDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@newDentists", newDentists);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+
+        [HttpDelete]
+        [Route("DeleteDentists")]
+        public JsonResult DeleteDentists(int id)
+        {
+            string query = "delete from dbo.dentists where id=@id";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("dentistappDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+
+
+    }
+}
---
+++ DentistSystem/DentistSystem/Controllers/PatientController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DentistSystem.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PatientController : ControllerBase
+	{
+		private IConfiguration _configuration;
+
+		public PatientController(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		[HttpGet]
+		[Route("GetPatients")]
+		public JsonResult GetPatients()
+		{
+			string query = "select * from dbo.patients";
+			DataTable table = new DataTable();
+			string sqlDatasource = _configuration.GetConnectionString("dentistappDBCon");
+			SqlDataReader myReader;
+			using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+			{
+				myCon.Open();
+				using (SqlCommand myCommand = new SqlCommand(query, myCon))
+				{
+					myReader = myCommand.ExecuteReader();
+					table.Load(myReader);
+					myReader.Close();
+					myCon.Close();
+				}
+			}
+
+			return new JsonResult(table);
+		}
+
+	}
+}
