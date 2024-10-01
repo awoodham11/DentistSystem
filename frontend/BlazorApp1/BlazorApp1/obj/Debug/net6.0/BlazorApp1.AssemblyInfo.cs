711dcc2ef5604c58cb86506e387bd85e0bdd7d00
awoodham11 <alexwoodham08@gmail.com>
2024-10-01 12:56:23 +0100

    added frontend user pages

--- frontend/BlazorApp1/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
+++ frontend/BlazorApp1/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fd3f86ce80cc34ffe4ab083dc20440fea41afcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
