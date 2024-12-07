d3abff823d180fbea84991d1873b70dde49f8f73
awoodham11 <alexwoodham08@gmail.com>
2024-12-07 20:05:50 +0000

    changed dotnet version for frontend and backend

--- DentistSystem/DentistSystem/Controllers/ReceptionistController.cs
+++ DentistSystem/DentistSystem/Controllers/ReceptionistController.cs
@@ -41,6 +41,54 @@ namespace DentistSystem.Controllers
             return new JsonResult(table);
         }
 
+		[HttpGet]
+		[Route("GetAppointments")]
+		public JsonResult GetAppointment()
+		{
+			string query = "select * from dbo.appointment";
+			DataTable table = new DataTable();
+			string sqlDatasource = _configuration.GetConnectionString("dentistappDBCon");
+			SqlDataReader myReader;
+			using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+			{
+				myCon.Open();
+				using (SqlCommand myCommand = new SqlCommand(query, myCon))
+				{
+					myReader = myCommand.ExecuteReader();
+					table.Load(myReader);
+					myReader.Close();
+					myCon.Close();
+				}
+			}
 
-    }
+			return new JsonResult(table);
+		}
+
+
+        [HttpGet]
+        [Route("GetPatientAppointment")]
+        public JsonResult GetPatientAppointment()
+        {
+			string query = "select * from dbo.appointment";
+			DataTable table = new DataTable();
+			string sqlDatasource = _configuration.GetConnectionString("dentistappDBCon");
+			SqlDataReader myReader;
+			using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+			{
+				myCon.Open();
+				using (SqlCommand myCommand = new SqlCommand(query, myCon))
+				{
+					myReader = myCommand.ExecuteReader();
+					table.Load(myReader);
+					myReader.Close();
+					myCon.Close();
+				}
+			}
+
+			return new JsonResult(table);
+		}
+        
+
+
+	}
 }
--- frontend/BlazorApp1/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
+++ frontend/BlazorApp1/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d7b15d51a9acc5447cecc6d8cb5589edb5c446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50bed74d17650bce4c3cdc70534855f8f1121a0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
